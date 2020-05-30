494133301ac4e29dab802b68739045657cc548d9
alehad <64356216+alehad@users.noreply.github.com>
2020-05-30 17:49:23 +0100

    added run/debug annotation to main method

--- Test1/src/alehad/test1/Test1Main.java
+++ Test1/src/alehad/test1/Test1Main.java
@@ -1,8 +1,8 @@
 package alehad.test1;
 
 public class Test1Main {
-
-	public static void main(String[] args) {
+	
+	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 	}
