1e24c6458f3a6016617b89f82071a1b0f1b301e3
alehad <64356216+alehad@users.noreply.github.com>
2020-05-30 18:18:18 +0100

    added Yeaah!

--- Test1/src/alehad/test1/Test1Main.java
+++ Test1/src/alehad/test1/Test1Main.java
@@ -4,7 +4,7 @@ public class Test1Main {
 	
 	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Yeeaah!");
 	}
 
 }
